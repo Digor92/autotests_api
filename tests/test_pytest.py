0aa29c5ff409440b1d5ab57c214089b78e11bd80
Digor92 <dedkovigor92@yandex.ru>
2025-10-27 22:23:18 +0300

    введение в pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,16 @@
+def test_first_try():
+    print('hello world')
+
+def test_second_try():
+    pass
+class TestUserLogin:
+    def test_one(self):
+        pass
+    def test_rwo(self):
+        pass
+
+def test_assert_positive_case():
+    assert (2+2) == 4
+
+def test_assert_negative_case():
+    assert (2+2) == 5, "error!!!"
