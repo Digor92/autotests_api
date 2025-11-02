b6c5c4a6145cac888b0aff5af63c76f097f6e0ce
Digor92 <dedkovigor92@yandex.ru>
2025-11-02 15:24:30 +0300

    Написание API автотестов используя Pytest AUTHENTICATION

---
+++ tests/test_authentication.py
@@ -0,0 +1,35 @@
+from http import HTTPStatus
+
+from clients.authentication.authentication_client import get_authentication_client
+from clients.authentication.authentication_schema import loginRequestSchema, loginResponseSchema
+from clients.users.public_users_client import get_public_user_client
+from clients.users.users_schema import CreateUserRequestSchema
+from tools.assertions.authentication import assert_login_response
+from tools.assertions.base import assert_status_code
+from tools.assertions.schema import validate_json_schema
+
+
+def test_login():
+    # инициализируется публичный клиент
+    public_users_client = get_public_user_client()
+    # инициализируется аутентификация
+    authentication_client = get_authentication_client()
+
+    # запрос создание пользователя
+    create_user_request = CreateUserRequestSchema()
+    public_users_client.create_user_api(create_user_request)
+
+    # запрос аутентификации пользователя
+    login_request = loginRequestSchema(
+        email = create_user_request.email,
+        password = create_user_request.password
+    )
+
+    login_response = authentication_client.login_api(login_request)
+    login_response_data = loginResponseSchema.model_validate_json(login_response.text)
+    assert_status_code(login_response.status_code, HTTPStatus.OK)
+    assert_login_response(login_response_data)
+    # Данная строчка кода выполняет валидацию JSON-данных против JSON Schema, полученной из Pydantic модели.
+    validate_json_schema(login_response.json(), login_response_data.model_json_schema())
+
+
--- tools/assertions/base.py
+++ tools/assertions/base.py
@@ -1,5 +1,12 @@
 from typing import Any
 def assert_status_code(actual: int, expected: int):
+    """
+        Проверяет, что фактический статус-код ответа соответствует ожидаемому.
+
+        :param actual: Фактический статус-код ответа.
+        :param expected: Ожидаемый статус-код.
+        :raises AssertionError: Если статус-коды не совпадают.
+        """
     assert actual == expected, (
         'incorrect response status code',
         f'expected status code{expected}',
@@ -7,8 +14,29 @@ def assert_status_code(actual: int, expected: int):
     )
 
 def assert_equal(actual: Any,expected: Any, name: str):
+    """
+        Проверяет, что фактическое значение равно ожидаемому.
+
+        :param name: Название проверяемого значения.
+        :param actual: Фактическое значение.
+        :param expected: Ожидаемое значение.
+        :raises AssertionError: Если фактическое значение не равно ожидаемому.
+        """
     assert actual == expected, (
         f'incorrect value: {name}',
         f'expected value: {expected}',
         f'actual value: {actual}'
+    )
+
+def assert_is_true(actual: Any, name: str):
+    """
+       Проверяет, что фактическое значение является истинным.
+
+       :param name: Название проверяемого значения.
+       :param actual: Фактическое значение.
+       :raises AssertionError: Если фактическое значение ложно.
+       """
+    assert actual, (
+        f'incorrent value: {name}',
+        f'ezpected true but got: {actual}'
     )
